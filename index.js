10c2c3457ab08112966a4c86a9419c718b79f585
Cody Marx Bailey <superphly@gmail.com>
2023-10-11 09:16:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const { Bip39, Random } = require("@cosmjs/crypto");
-const { DirectSecp256k1HdWallet } = require("@cosmjs/proto-signing");
+import { mkdirSync, createWriteStream } from "fs";
+const { Console } = console; 
+import { Bip39, Random } from "@cosmjs/crypto";
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 
-const NUM_WALLETS = process.env.NUM_WALLETS ?? 15;
+// 
+const NUM_WALLETS = process.argv[2] ?? 10;
+const W_NAME = process.argv[3] ?? 'error';
+
+
+// Create wallets dir for output files
+mkdirSync(`./wallets/public`, {recursive: true});
+mkdirSync(`./wallets/private`, {recursive: true});
+
+// File Outputs
+const privFile = createWriteStream(`./wallets/private/${W_NAME}-privkeys`);
+const pubFile = createWriteStream(`./wallets/public/${W_NAME}-pubkeys`);
+
+// Helpers to Write to filesystem
+const privOut = new Console({ stdout: privFile });
+const pubOut = new Console({ stdout: pubFile });
+
+// getBytes Length
 const length = 4 * Math.floor((11 * 24) / 33);
 
 async function generateWallet() {
@@ -12,15 +31,24 @@ async function generateWallet() {
   });
   const address = (await wallet.getAccounts())[0].address;
 
-  return [mnemonic, address];
+  return {mnemonic, address};
 }
 
 async function start() {
+  
+  const w = [];
+
   for (let i = 0; i < NUM_WALLETS; i++) {
-    const [mnemonic, address] = await generateWallet();
-    console.log(`Mnemonic: ${mnemonic}`);
-    console.log(`Address: ${address}`);
-    console.log(`\n`);
+    w.push(await generateWallet());
+  }
+
+  for (let i = 0; i < NUM_WALLETS; i++) {
+    let zi = ('00'+i).slice(-2);
+    let privLine = `Wallet ${W_NAME} #${zi}\nMnemonic:\t${w[i].mnemonic}\nAddress:\t${w[i].address}\n`;
+    let pubLine = `Wallet ${W_NAME} #${zi}\t${w[i].address}`;
+    privOut.log(privLine);
+    pubOut.log(pubLine);
+    console.log(pubLine);
   }
 }
 
